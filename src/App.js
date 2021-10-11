58799d442c73fc0117a305f5fd66f1d8b7abd303
codecaiine <akabrouyannickn@gmail.com>
2021-10-11 23:49:59 +0000

    Add linters for react and redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Group project</h1>
+        <h1>Group project</h1>
       </header>
     </div>
   );
