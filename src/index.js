a12e1a9eef189a594b03acbf14cf329674372cb7
codecaiine <akabrouyannickn@gmail.com>
2021-10-12 13:40:56 +0000

    Import correct navbar component in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
