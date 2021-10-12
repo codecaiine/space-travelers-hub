9c0ea0352deeb5a7be41a85e469f11fbfed3b746
codecaiine <akabrouyannickn@gmail.com>
2021-10-12 00:03:09 +0000

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider />
   </React.StrictMode>,
   document.getElementById('root'),
 );
