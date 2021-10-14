209b092b1f00cfc98a6d6a4b594b7a1544db18bc
codecaiine <akabrouyannickn@gmail.com>
2021-10-14 07:54:06 +0000

    Install prop types and import it in SingleRocket component

---
+++ src/components/SingleRocket.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './rocket.css';
+
+const SingleRocket = ({
+  name,
+  description,
+  image,
+}) => (
+  <div className="each-rocket">
+    <div className="rocket-img">
+      <img src={image} alt={name} className="img-size" />
+    </div>
+    <div className="rocket-details">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <button type="submit" className="rocket-reserve-btn">Reserve Rocket</button>
+    </div>
+  </div>
+);
+
+SingleRocket.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.isRequired,
+  image: PropTypes.isRequired,
+};
+
+export default SingleRocket;
