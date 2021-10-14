40c677866f0a498a13eccb72e31646c36e6ee3b7
codecaiine <akabrouyannickn@gmail.com>
2021-10-14 08:01:21 +0000

    Import SingleRocket in Rocket page

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import SingleRocket from '../components/SingleRocket';
 
-const Rockets = () => (
-  <></>
-);
+const Rockets = () => {
+  const list = useSelector((state) => state.rocketsReducer);
+  return (
+    <>
+      <ul>
+        {list.map((each) => (
+            <SingleRocket
+            key={each.id}
+            name={each.rocketName}
+            description={each.description}
+            image={each.flickrImages}
+          />
+        ))}
+      </ul>
+    </>
+   );
+  };
 
 export default Rockets;
